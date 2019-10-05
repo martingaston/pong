5ad3908d660b0c1e1ff8d07e9b8442bb37bb0b91
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-10-05 21:10:42 +0100

    add pong2 /w screenshake and speed increase
    increase speed of ball
    
    increase paddle size and add more shake as rally increases
    
    create pong2
    
    remove pong1
    
    remove pong1/moonshine
    
    add moonshine to pong1

--- pong2/main.lua
+++ pong2/main.lua
@@ -2,6 +2,7 @@ screenX, screenY = love.graphics.getDimensions()
 font = love.graphics.newFont("assets/fonts/sharp-retro.ttf", 96, "mono")
 local moonshine = require 'moonshine'
 local flux = require 'flux'
+local shack = require 'shack'
 
 function love.load()
   love.window.setTitle("PONG")
@@ -9,7 +10,7 @@ function love.load()
 
   playerOne = {}
   playerOne.width = 10
-  playerOne.height = 60
+  playerOne.height = 90
   playerOne.y = screenY*.5 - playerOne.height*.5
   playerOne.x = 20
   playerOne.speed = 1200
@@ -17,7 +18,7 @@ function love.load()
 
   playerTwo = {}
   playerTwo.width = 10
-  playerTwo.height = 60
+  playerTwo.height = 90
   playerTwo.y = screenY*.5 - playerTwo.height*.5
   playerTwo.x = screenX - playerTwo.width - 20
   playerTwo.speed = 1200
@@ -32,10 +33,13 @@ function love.load()
   ball.x = screenX*.5 - ball.width*.5
   ball.y = screenY*.5 - ball.height*.5
 
+  shake = 3
+
   divider = {}
   divider.width = 10
   divider.height = 10
 
+  shack:setDimensions(love.graphics.getDimensions())
   crt = moonshine(moonshine.effects.crt)
   background = moonshine(moonshine.effects.glow).chain(moonshine.effects.scanlines)
   foreground = moonshine(moonshine.effects.glow)
@@ -44,6 +48,7 @@ end
 
 function love.update(dt)
   flux.update(dt)
+  shack:update(dt)
 
   if love.keyboard.isDown("a") then
     playerOne.y = math.max(0, playerOne.y - playerOne.speed * dt)
@@ -73,15 +78,19 @@ function love.update(dt)
   if collides(ball, playerOne) then
     ball.x = playerOne.x + playerOne.width + 1
     ball.dx = ball.dx * -1
-    flux.to(ball, 0.1, { width = ball.width * 1.25, height = ball.height * 1.25 }):ease("circout"):after(ball, 0.25, { width = ball.width, height = ball.height })
-    flux.to(divider, 0.1, { width = divider.width * 1.1, height = divider.height * 1.1 }):after(divider, 0.1, { width = divider.width, height = divider.height})
+    flux.to(ball, 0.1, { dx = ball.dx * 3, width = ball.width * 1.25, height = ball.height * 1.25 }):ease("circout"):after(ball, 0.25, { dx = math.min(750, ball.dx * 1.25), width = ball.width, height = ball.height })
+    flux.to(divider, 0.1, { width = divider.width * 2, height = divider.height * 1.1 }):after(divider, 0.1, { width = divider.width, height = divider.height})
+    shack:setShake(shake)
+    shake = shake + 1
   end
 
   if collides(ball, playerTwo) then
     ball.x = playerTwo.x - ball.width - 1
     ball.dx = ball.dx * -1
-    flux.to(ball, 0.1, { width = ball.width * 1.25, height = ball.height * 1.25 }):ease("circout"):after(ball, 0.25, { width = ball.width, height = ball.height })
-    flux.to(divider, 0.1, { width = divider.width * 1.1, height = divider.height * 1.1 }):after(divider, 0.1, { width = divider.width, height = divider.height})
+    flux.to(ball, 0.1, { dx = ball.dx * 3, width = ball.width * 1.25, height = ball.height * 1.25 }):ease("circout"):after(ball, 0.25, { dx = math.min(750, ball.dx * 1.25), width = ball.width, height = ball.height })
+    flux.to(divider, 0.1, { width = divider.width * 2, height = divider.height * 1.1 }):after(divider, 0.1, { width = divider.width, height = divider.height})
+    shack:setShake(shake)
+    shake = shake + 1
   end
 
   if ball.x + ball.width > screenX then
@@ -90,6 +99,7 @@ function love.update(dt)
     ball.dx = 120
     ball.x = screenX*.5 - ball.width*.5
     ball.y = screenY*.5 - ball.height*.5
+    shake = 3
   end
 
   if ball.x < 0 then
@@ -98,6 +108,7 @@ function love.update(dt)
     ball.dx = 120
     ball.x = screenX*.5 - ball.width*.5
     ball.y = screenY*.5 - ball.height*.5
+    shake = 3
   end
 end
 
@@ -109,6 +120,7 @@ function collides(a, b)
 end
 
 function love.draw()
+  shack:apply()
   crt(function()
     love.graphics.setColor(51 / 100, 124 / 100, 160 / 100, 0.1)
     love.graphics.rectangle('fill', 0, 0, screenX, screenY)
@@ -121,7 +133,7 @@ function love.draw()
       love.graphics.print(playerTwo.score, screenX*.5 + 60, 100)
 
       for i = 5, screenY, 20 do
-        love.graphics.rectangle('fill', screenX*.5 - 5, i, divider.width, divider.height)
+        love.graphics.rectangle('fill', screenX*.5 - divider.width*.5, i, divider.width, divider.height)
       end
 
       love.graphics.rectangle('fill', playerOne.x, playerOne.y, playerOne.width, playerOne.height)
